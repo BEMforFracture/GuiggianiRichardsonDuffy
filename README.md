aa2f8ec796f98927cd94da2829e689ee79da1efc
Adrien VET <adrien.vet@ensta-paris.fr>
2025-10-21 11:09:32 +0200

    doc

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # GuiggianiRichardsonDuffy
+
+[![Dev](https://img.shields.io/badge/docs-dev-blue.svg)](https://aguelord.github.io/GuiggianiRichardsonDuffy/dev)
+[![Stable](https://img.shields.io/badge/docs-stable-blue.svg)](https://aguelord.github.io/GuiggianiRichardsonDuffy/stable)
+
 Here is a repo linked to a potential article about implementation of a modern version of the Guiggiani algorithm based on 
 
 1) Richardson extrapolation for good accuracy of the Laurent's coefficients
--- docs/make.jl
+++ docs/make.jl
@@ -6,19 +6,20 @@ using GuiggianiRichardsonDuffy
 
 # Options de build
 makedocs(
-    sitename = "GuiggianiRichardsonDuffy.jl",
-    modules = [GuiggianiRichardsonDuffy],
-    format = Documenter.HTML(; prettyurls = false),
-    pages = [
-        "Accueil" => "index.md",
-        "Guide"   => "guide.md",
-        "API"     => "api.md",
-    ],
-    doctest = true,
-    warnonly = [:missing_docs, :docs_block],  # Permet de construire malgré les docstrings manquantes
+	sitename = "GuiggianiRichardsonDuffy.jl",
+	modules = [GuiggianiRichardsonDuffy],
+	format = Documenter.HTML(; prettyurls = false),
+	pages = [
+		"Accueil" => "index.md",
+		"Guide"   => "guide.md",
+		"API"     => "api.md",
+	],
+	doctest = true,
+	warnonly = [:missing_docs, :docs_block],  # Permet de construire malgré les docstrings manquantes
 )
 
-# Déploiement (optionnel, à activer une fois sur GitHub)
-# deploydocs(
-#     repo = "github.com/<user>/GuiggianiRichardsonDuffy.jl.git",
-# )
+# Déploiement sur GitHub Pages
+deploydocs(
+    repo = "github.com/Aguelord/GuiggianiRichardsonDuffy",
+    devbranch = "main",
+)
